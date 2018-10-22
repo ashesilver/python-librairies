77604c6c32ae80a82ae00c68b5d04aa9dbf010f4
thib ters <43893686+ashesilver@users.noreply.github.com>
2018-10-22 11:10:51 +0200

    Update takeInfo.py

--- takeInfo.py
+++ takeInfo.py
@@ -1,17 +1,12 @@
 #!/usr/bin/python3
 
-def do(labonnebonnestring):
-	labonnebonnestring+=" "
-	liH=[]
+def do(labonnebonnestring, liH=[]):
 	while labonnebonnestring != "":
-		i=0
-		temp = ""
-		x=""
+		temp,x,i = "","",0
 		while x!=" ":
 			temp += labonnebonnestring[i]
 			x = labonnebonnestring[i+1]
 			i+=1
-
 		liH.append(temp)
 		labonnebonnestring = labonnebonnestring[len(temp)+1:]
 	return liH
