fccea197f3b5f0fe456c34dc4ffb825eec563eb8
thib ters <43893686+ashesilver@users.noreply.github.com>
2018-11-16 19:01:42 +0100

    dqesfv

--- bases.py
+++ bases.py
@@ -9,7 +9,10 @@ class Number():
 	def __init__(self, ref, state=10):
 		self.ref = ref
 		self.duplicata = None
-		self.indx = {"numbers":('0','1','2','3','4','5','6','7','8','9','10','11','12','13','14','15','16','17','18','19','20','21','22','23','24','25','26','27','28','29','30','31','32','33','34','35'),"letters":('0','1','2','3','4','5','6','7','8','9','a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z')}
+		self.indx = {
+		"numbers":('0','1','2','3','4','5','6','7','8','9','10','11','12','13','14','15','16','17','18','19','20','21','22','23','24','25','26','27','28','29','30','31','32','33','34','35','36','37'),
+		"letters":('0','1','2','3','4','5','6','7','8','9','a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z',' ','\'')
+		}
 		self.state = state
 	
 
@@ -26,10 +29,13 @@ class Number():
 		for i in range(0,len(a)):
 			b=self.state*b+int(self.indx["numbers"][self.indx["letters"].index(a[i])])
 		self.duplicata= b
+		self.reverse()
 		self.state = 10
 	
 
 	def base(self, base):
+		if self.state != 10:
+			self.dec()
 		
 		self.duplicata = ""
 		a = self.ref
@@ -40,6 +46,7 @@ class Number():
 			a = int (a / base)
 			self.duplicata+=b
 		self.duplicata = ''.join(reversed(self.duplicata))
+		self.reverse()
 		self.state = base
 	
 
@@ -50,11 +57,7 @@ class Number():
 			for x in (self, other):
 				if not x.state == 10 :
 					x.dec()
-					x.reverse()
 			a = self+other
-			for x in (self, other):
-				if not x.state == 10 :
-					x.reverse()
 			return a
 
 	def __sub__(self, other):
@@ -64,11 +67,7 @@ class Number():
 			for x in (self, other):
 				if not x.state == 10 :
 					x.dec()
-					x.reverse()
 			a = self-other
-			for x in (self, other):
-				if not x.state == 10 :
-					x.reverse()
 			return a
 
 	def __repr__(self):
--- brainfuck.py
+++ brainfuck.py
@@ -2,6 +2,7 @@
 # -*- coding:utf-8 -*-
 
 def helper():
+	print("not done yet")
 	pass
 
 class Code(object):
