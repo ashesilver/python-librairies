eb16f39c5144d59b6bf696b0e874dcf8320e129b
thib ters <43893686+ashesilver@users.noreply.github.com>
2018-11-25 17:05:17 +0100

    Create algoLang.py

---
+++ algoLang.py
@@ -0,0 +1,22 @@
+#!/bin/python3
+# -*- coding : utf-8 -*-
+
+
+def cond(a,b,c):
+	return b if a else c
+
+def liVide():
+	return []
+
+def estVide(a):
+	return True if a==[] else False
+
+def tete(a):
+	return a[1:]
+
+def queue(a):
+	return a[0]
+
+def cons(a,b):
+	return b.insert(0,a)
+
