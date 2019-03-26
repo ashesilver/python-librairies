fc013233dcb6866adfa3a79087b00f374b98c8a5
thib ters <43893686+ashesilver@users.noreply.github.com>
2019-03-26 18:49:54 +0100

    update takeinfo

--- takeInfo.py
+++ takeInfo.py
@@ -1,14 +1,27 @@
-#!/usr/bin/python3
+#!/usr/binp/python3
 
-def do(spacingFlag =" "):
+def do(inp,spacingFlag =" "):
 	liH=[]
-	labonnebonnestring += " "
-	while labonnebonnestring != "":
+	inp += spacingFlag
+	while inp != "":
 		temp,x,i = "","",0
 		while x!=spacingFlag:
-			temp += labonnebonnestring[i]
-			x = labonnebonnestring[i+1]
+			temp += inp[i]
+			x = inp[i+1]
 			i+=1
 		liH.append(temp)
-		labonnebonnestring = labonnebonnestring[len(temp)+1:]
+		inp = inp[len(temp)+1:]
 	return liH
+
+def optimized(inp,flag=" ",conversor=False):
+	li = [""]
+	count = 0
+	for x in inp :
+		if x!=flag :
+			li[count]+=x
+		else :
+			count+=1
+			li.append("")
+	if conversor :
+		return [int(x) for x in li if x!='']
+	return [x for x in li if x!='']
